0cbd9d225391430b44f7f10dde9f6e97ad6750ca
Prathamesh <prathameshpadiyar@gmail.com>
2025-06-03 14:58:55 +0530

    constants for project

---
+++ include/dupesweep/constants.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstddef>
+
+namespace dupesweep {
+
+    /*
+        buffer size for quick hash,
+        ie hash these many bytes first. 
+        if they don't match, file is obviously not a duplicate
+    */
+    constexpr size_t QUICK_HASH_BYTES = 1024;
+
+    // similar 4MB buffer for full hash
+    constexpr size_t HASH_BUFFER_SIZE = 4 * 1024 * 1024;
+
+    constexpr int DEFAULT_THREAD_COUNT = 4;
+}
